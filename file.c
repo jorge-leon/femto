1dab404c93693141bdc7b9b4c253a16003b95f77
Georg Lehner <jorge@at.anteris.net>
2025-08-28 09:07:33 +0200

    file extension. add feof, fgets, popen, pclose

--- file.c
+++ file.c
@@ -1,4 +1,6 @@
 #include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "lisp.h"
 #include "file.h"
@@ -6,7 +8,11 @@
 Primitive flisp_file_primitives[] = {
     {"fflush", 1, 1, TYPE_STREAM, primitiveFflush},
     {"ftell",  1, 1, TYPE_STREAM, primitiveFtell},
+    {"feof",   1, 1, TYPE_STREAM, primitiveFeof},
     {"fgetc",  1, 1, TYPE_STREAM, primitiveFgetc},
+    {"fgets",  1, 1, TYPE_STREAM, primitiveFgets},
+    {"popen",  2, 2, TYPE_STRING, primitivePopen},
+    {"pclose", 1, 1, TYPE_STREAM, primitivePclose},
 };
 
 /** file_fflush - flush output stream
@@ -39,6 +45,12 @@ Object *primitiveFtell(Interpreter *interp, Object** args, Object **env)
         exception(interp, invalid_value, "(ftell stream) - stream already closed");
     return newInteger(interp, file_ftell(interp, FLISP_ARG_ONE));
 }
+Object *primitiveFeof(Interpreter *interp, Object** args, Object **env)
+{
+    if (FLISP_ARG_ONE->fd == NULL)
+        exception(interp, invalid_value, "(feof stream) - stream already closed");
+    return (feof(FLISP_ARG_ONE->fd)) ? t : nil;
+}
 
 Object *primitiveFgetc(Interpreter *interp, Object** args, Object **env)
 {
@@ -49,6 +61,54 @@ Object *primitiveFgetc(Interpreter *interp, Object** args, Object **env)
         return nil;
     s[0] = (char)c;
     return newString(interp, s);
+}
+Object *primitiveFgets(Interpreter *interp, Object** args, Object **env)
+{
+    Object *string = nil;
+    char *input;
+
+    if (FLISP_ARG_ONE->fd == NULL)
+        exception(interp, invalid_value, "(fgets stream) - stream already closed");
+
+    input = malloc(INPUT_FMT_BUFSIZ);
+    if(input == NULL)
+        exception(interp, out_of_memory, "fgets() failed, %s", strerror(errno));
+
+    *input = '\0';
+
+    if(fgets(input, INPUT_FMT_BUFSIZ, FLISP_ARG_ONE->fd) != NULL) {
+        string = newString(interp, input);
+        free(input);
+        return string;
+    }
+    free(input);
+    if (!feof(FLISP_ARG_ONE->fd))
+        exceptionWithObject(interp, FLISP_ARG_ONE, io_error, "fgetc() failed: %s", strerror(errno));
+    return end_of_file;
+}
+
+Object *primitivePopen(Interpreter *interp, Object** args, Object **env)
+{
+    FILE *fd;
+
+    if (strcmp(FLISP_ARG_TWO->string, "r") && strcmp(FLISP_ARG_TWO->string, "w"))
+         exception(interp, invalid_value,
+                   "(popen path mode) - mode must be \"r\" or \"w\", got: %s", FLISP_ARG_TWO->string);
+
+    fd = popen(FLISP_ARG_ONE->string, FLISP_ARG_TWO->string);
+    if (fd == NULL)
+        exception(interp, io_error, "popen() failed: %s", strerror(errno));
+
+    return newStreamObject(interp, fd, FLISP_ARG_ONE->string);
+}
+Object *primitivePclose(Interpreter *interp, Object** args, Object **env)
+{
+    int result = pclose(FLISP_ARG_ONE->fd);
+
+    if (result == -1)
+        exceptionWithObject(interp, FLISP_ARG_ONE, io_error, "pclose() failed: %s", strerror(errno));
+
+    return newInteger(interp, result);
 }
 
 /*
--- file.h
+++ file.h
@@ -7,7 +7,11 @@ extern Primitive flisp_file_primitives[];
 
 extern Object *primitiveFflush(Interpreter *, Object** , Object **);
 extern Object *primitiveFtell(Interpreter *, Object** , Object **);
+extern Object *primitiveFeof(Interpreter *, Object** , Object **);
 extern Object *primitiveFgetc(Interpreter *, Object** , Object **);
+extern Object *primitiveFgets(Interpreter *, Object** , Object **);
+extern Object *primitivePopen(Interpreter *, Object** , Object **);
+extern Object *primitivePclose(Interpreter *, Object** , Object **);
 
 #endif
 /*

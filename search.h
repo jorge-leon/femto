11ad7f299e28a9e3a21894e30a1126e7c671b97b
Georg Lehner <jorge@lifebook.magma.intern>
2025-12-14 23:18:31 +0100

    Factor out search.h

---
+++ search.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+extern point_t search_backwards(char *);
+extern point_t search_forward(char *);
+extern void move_to_search_result(point_t);
+extern void search(void);
+
+
+
+#endif
